c499b9598e2eb924618d4d03e19c962fc3c66dba
Yu Qin <qinyu1119@yahoo.com>
2016-12-07 13:57:03 -0500

    Yu Qin
    VAR  TPR&FPR

--- lib/VAR.R
+++ lib/VAR.R
@@ -64,3 +64,54 @@ for(i in 1:100)
 {d1_all[i]<-ifelse(label_num[1863+i]>label_num[1862+i],1,0)
 d2_all[i]<-ifelse(yhat_all_once[i]>label_num[1862+i],1,0)}
 sum(d1==d2)/length(d1)
+
+
+
+#VAR plot#
+
+data_temp<-rbind(data_3_new,M_3)
+data_temp_all<-cbind(data_temp,label_num)
+upper<-vector(length=126)
+lower<-vector(length=126)
+y.predict<-vector(length=126)
+for(i in 1:126)
+{  
+#var_temp<-vector(length=dim(data_temp)[2])
+#for(j in 1:length(var_temp))
+#{ var_temp[j]<-var(data_temp[c(1:1862+i),j])}
+#var_top<-VAR(data_temp[c(1:1862+i),order(var_temp,decreasing=T)[1:10]],p=2,type="both")
+data_pca<-prcomp(t(data_temp[c(1:(1862+i)),]))$rotation[,c(1:10)]
+data_use<-cbind(data_pca,label_num[1:(1862+i)])
+var_top<-VAR(data_use,p=2,type="both")
+yhat_top<-predict(var_top,n.ahead=1,ci=0.95)
+upper[i]<-yhat_top$fcst$X[1,3]
+lower[i]<-yhat_top$fcst$X[1,2]
+y.predict[i]<-yhat_top$fcst$X[1,1]
+}
+date<-rownames(M_3)
+value_true<-label_num[1864:1989]
+y.pred<-data.frame(date,value_true,upper,lower,y.predict)
+y.pred$date<-as.Date(y.pred$date,format="%Y-%m-%d")
+library(ggplot2)
+ggplot(y.pred, aes(date)) + 
+  geom_line(aes(y = value_true, colour = "True value")) + 
+  #geom_line(aes(y = upper, colour = "var1"))+
+  #geom_line(aes(y = lower, colour = "var1"))+
+  geom_line(aes(y = y.predict, colour = "Predict value"))+
+  ylab("Daily Adj Close Index")+ggtitle("VAR model(3 grams) Prediction of Adj Close Index")
+
+#distance#
+value_true_dis<-vector(length=125)
+for(i in 1:125)
+{value_true_dis[i]<-value_true[i+1]-value_true[i]}
+pred.dis<-vector(length=125)
+for(i in 1:125)
+{pred.dis[i]<-y.predict[i+1]-value_true[i]}
+v<-ifelse(value_true_dis>=0,1,0)
+p<-ifelse(pred.dis>=0,1,0)
+
+
+ggplot(df,aes(FPR,TPR,color=GeneSet))+geom_line(size = 2, alpha = 0.7)+
+  labs(title= "ROC curve", 
+       x = "False Positive Rate (1-Specificity)", 
+       y = "True Positive Rate (Sensitivity)")
